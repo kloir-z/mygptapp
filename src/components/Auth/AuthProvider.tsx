e131657a2d82222459dc1d9da1c873bd908e62a9
kloir-z <129265849+kloir-z@users.noreply.github.com>
2023-08-02 13:34:30 +0900

    Update AuthProvider.tsx
    gpt3.5

--- src/components/Auth/AuthProvider.tsx
+++ src/components/Auth/AuthProvider.tsx
@@ -11,26 +11,31 @@ interface AuthProviderProps {
 export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
   const [user, setUser] = useState<User | null>(null);
 
-  useEffect(() => {
-    const auth = firebase.auth();
-    const provider = new firebase.auth.GoogleAuthProvider();
+useEffect(() => {
+  const auth = firebase.auth();
+  const provider = new firebase.auth.GoogleAuthProvider();
 
-    auth.onAuthStateChanged((currentUser) => {
-      if (currentUser) {
-        setUser(currentUser);
-      } else {
-        auth.signInWithRedirect(provider);
-      }
-    });
+  const handleAuthStateChanged = (currentUser) => {
+    if (currentUser) {
+      setUser(currentUser);
+    } else {
+      auth.signInWithRedirect(provider);
+    }
+  };
 
-    auth.getRedirectResult().then((result) => {
-      if (result.user) {
-        setUser(result.user);
-      }
-    }).catch((error) => {
-      console.error(error);
-    });
-  }, []);
+  const handleRedirectResult = (result) => {
+    if (result.user) {
+      setUser(result.user);
+    }
+  };
+
+  auth.onAuthStateChanged(handleAuthStateChanged);
+  auth.getRedirectResult().then(handleRedirectResult).catch(console.error);
+
+  return () => {
+    auth.onAuthStateChanged(handleAuthStateChanged);
+  };
+}, [firebase.auth, firebase.auth.GoogleAuthProvider]);
 
   return (
     <AuthContext.Provider value={{ user, setUser }}>
